d303d11210d11a421a16e7dd36d806b9612af41a
Vladimir Kizelbashev <vk.fstor@gmail.com>
2020-04-17 23:20:24 +0400

    Add vote entity

---
+++ src/main/java/com/vkiz/voting/model/Vote.java
@@ -0,0 +1,86 @@
+package com.vkiz.voting.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.vkiz.voting.HasDate;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "votes", uniqueConstraints = {@UniqueConstraint(columnNames =
+        {"date", "user_id"}, name = "votes_unique_date_user_idx")})
+public class Vote extends AbstractBaseEntity implements HasDate {
+
+    @Column(name = "date", nullable = false)
+    @NotNull
+    private LocalDate date = LocalDate.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull
+    private Restaurant restaurant;
+
+    public Vote() {
+    }
+
+    public Vote(Vote v) {
+        this(v.getId(), v.getDate(), v.getUser(), v.getRestaurant());
+    }
+
+    public Vote(Integer id, LocalDate date, User user, Restaurant restaurant) {
+        super(id);
+        this.date = date;
+        this.user = user;
+        this.restaurant = restaurant;
+    }
+
+    @Override
+    public LocalDate getDate() {
+        return date;
+    }
+
+    @Override
+    public void setDate(LocalDate dateTime) {
+        this.date = dateTime;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    @Override
+    public String toString() {
+        return "Vote{" +
+                "id=" + id +
+                '}';
+    }
+}
