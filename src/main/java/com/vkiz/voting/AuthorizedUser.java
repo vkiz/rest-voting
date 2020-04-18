d2e75bd862c2b6d60f677b7dcdd426c5e87ce8d4
Vladimir Kizelbashev <vk.fstor@gmail.com>
2020-04-18 23:35:49 +0400

    Add authorized user

---
+++ src/main/java/com/vkiz/voting/AuthorizedUser.java
@@ -0,0 +1,32 @@
+package com.vkiz.voting;
+
+import com.vkiz.voting.model.User;
+
+public class AuthorizedUser extends org.springframework.security.core.userdetails.User {
+
+    private static final long serialVersionUID = 1L;
+
+    private User user;
+
+    public AuthorizedUser(User user) {
+        super(user.getEmail(), user.getPassword(), true, true, true, true, user.getRoles());
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return user.getId();
+    }
+
+    public void update(User newUser) {
+        user = newUser;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String toString() {
+        return user.toString();
+    }
+}
